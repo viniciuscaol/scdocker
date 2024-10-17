3e551592db564385dca06759c40a38a0ca54ac1e
Vinicius Caldas <viniciuscaol@gmail.com>
2024-10-16 22:07:40 -0300

    fix: correção do projeto

--- aula07/back/app.mjs
+++ aula07/back/app.mjs
@@ -1,11 +1,14 @@
 import express from 'express';       // Importação correta do Express (ESM)
 import mongodb from 'mongodb';       // Importação correta do MongoDB (CommonJS)
+import cors from 'cors';
 
 const { MongoClient } = mongodb;     // Desestruturação do MongoClient do módulo MongoDB
 
 const app = express();
 const port = 3000;
 const mongoUrl = 'mongodb://database-mongo:27017';
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     MongoClient.connect(mongoUrl, { useUnifiedTopology: true }, (err, client) => {
